1048b58d655f9e89adbafacbd099136fa36a84b6
AdamWojtczak <adamwojtczak807@gmail.com>
2020-10-13 14:56:34 +0200

    initial commit, implementation of ReverseString class

---
+++ PPKWU_zad1_rev/src/main/java/PPKWU_zad1_rev/PpkwuZad1RevApplication.java
@@ -0,0 +1,13 @@
+package PPKWU_zad1_rev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpkwuZad1RevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpkwuZad1RevApplication.class, args);
+	}
+
+}
---
+++ PPKWU_zad1_rev/src/test/java/PPKWU_zad1_rev/PpkwuZad1RevApplicationTests.java
@@ -0,0 +1,13 @@
+package PPKWU_zad1_rev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpkwuZad1RevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
