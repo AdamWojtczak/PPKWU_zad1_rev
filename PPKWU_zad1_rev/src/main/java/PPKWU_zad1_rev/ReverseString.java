d020c3a4b1dda44f0aba917ac56deb12d5a26b00
AdamWojtczak <adamwojtczak807@gmail.com>
2020-10-13 15:19:30 +0200

    ReverseString.reverseString now uses stringToBeReversed parameter

--- PPKWU_zad1_rev/src/main/java/PPKWU_zad1_rev/ReverseString.java
+++ PPKWU_zad1_rev/src/main/java/PPKWU_zad1_rev/ReverseString.java
@@ -1,32 +1,22 @@
 package PPKWU_zad1_rev;
-// Java program to ReverseString using ByteArray.
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.*;
-import java.io.*;
-import java.util.*;
-
-// Class of ReverseString
 @RestController
 public class ReverseString {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String reverseString(String input) {
-		// getBytes() method to convert string
-		// into bytes[].
-		input  = "aAabBb";
-		byte[] strAsByteArray = input.getBytes();
+	public String reverseString( @RequestParam("stringToBeReversed") String stringToBeReversed) {
+		byte[] strAsByteArray = stringToBeReversed.getBytes();
 
 		byte[] result = new byte[strAsByteArray.length];
-
-		// Store result in reverse order into the
-		// result byte[]
 		for (int i = 0; i < strAsByteArray.length; i++)
 			result[i] = strAsByteArray[strAsByteArray.length - i - 1];
 
-		System.out.println(new String(result));
 		return new String(result);
 	}
 }
